f072d9621cfff400bdb8e84010abb14a825a534a
jainl <deep.lakshya1310@gmail.com>
2023-02-02 01:48:25 +0530

    adapter

--- app/src/main/java/com/example/bakeit/view/adapters/CustomListItemAdapter.kt
+++ app/src/main/java/com/example/bakeit/view/adapters/CustomListItemAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.bakeit.view.adapters
 
 import android.app.Activity
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bakeit.databinding.ItemCustomListBinding
@@ -11,20 +12,25 @@ class CustomListItemAdapter(
     private val listItems: List<String>,
     private val selection: String)
     : RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>(){
+
     class ViewHolder(view: ItemCustomListBinding):RecyclerView.ViewHolder(view.root){
         val tvText = view.tvText
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val binding: ItemCustomListBinding = ItemCustomListBinding
+            .inflate(LayoutInflater.from(activity))
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = listItems[position]
+
+        holder.tvText.text = item
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listItems.size
     }
 }
